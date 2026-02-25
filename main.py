4bd5807fb51799850531c29ad1cfc95c19371a4f
Armaan Sandhu <103614886+armaan3722@users.noreply.github.com>
2026-02-24 17:02:31 -0700

    Main program loop and basic filament data viewer created

--- main.py
+++ main.py
@@ -6,7 +6,24 @@ import data as csvUtils
 csvList = ['./data/prints.csv', './data/filament-usage.csv', './data/all-rolls.csv']
 
 # Load csv files into dataframes
-df1, df2, df3 = csvUtils.readData(csvList)
+printHistory, filamentHistory, currentFilament = csvUtils.readData(csvList)
 
 # Export csv files from dataframes
-csvUtils.writeData(csvList, [df1, df2, df3])
+csvUtils.writeData(csvList, [printHistory, filamentHistory, currentFilament])
+
+# Main program loop
+runLoop = True
+while runLoop:
+    # Start action iteration
+    print('Would you like to view current filament(1), update current filament(2), add a print(3), or quit(4)?')
+    action = int(input())
+
+    if action == 1:
+        print(currentFilament)
+    elif action == 2:
+        print('2')
+    elif action == 3:
+        print('3')
+    elif action == 4:
+        print('Program ending')
+        runLoop = False
