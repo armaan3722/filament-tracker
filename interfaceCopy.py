d2a8776036e070e8beddce7695651232963310a0
Armaan Sandhu <103614886+armaan3722@users.noreply.github.com>
2026-03-02 08:51:47 -0700

    Implemented adding printers to csv files

--- interfaceCopy.py
+++ interfaceCopy.py
@@ -11,3 +11,36 @@ def readPrinter(printerPath, printerMaintenancePath):
     print(printer)
     print('\n\nPrinter maintenance history')
     print(printerMaintenance)
+
+    # Update
+    print('\n\nWould you like to add a printer(1), edit printer information(2), create maintenance event(3), or go back to home page(4)')
+    action = int(input())
+
+    match action:
+        case 1:
+            addPrinter(printer, printerPath)
+        case 2:
+            print(2)
+        case 3:
+            print(3)
+        case 4:
+            print('Returning to home page')
+
+def addPrinter(printer, path):
+    # Get new printer information
+    print('What is the new printer company')
+    newPrinterCompany = input()
+    print('What is the new printer model')
+    newPrinterModel = input()
+    print('What is the new printer name')
+    newPrinterName = input()
+    print('What is the new printer cost')
+    newPrinterCost = input()
+    print('What is the new printer date purchased')
+    newPrinterDate = input()
+
+    # Update dataframe
+    printer = csvUtils.addRow([len(printer),newPrinterName,newPrinterCompany,newPrinterModel,newPrinterDate,newPrinterCost,0], printer)
+
+    # Save
+    csvUtils.writeData([path], [printer])
